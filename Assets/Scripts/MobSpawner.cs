f7cee6678b318285f025578cf1ee0143ae79d837
Louis <louisemard17@gmail.com>
2020-09-17 16:08:29 +0200

    feat : working on zombie spawn and ultra basic movement system
    WIP #3

---
+++ Assets/Scripts/MobSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSpawner : MonoBehaviour
+{
+    public GameObject[] spawnPoints;
+    public GameObject[] availableEntities;
+
+    // Waves related stuff
+
+    public int numberOfWaves = 5;
+    public int currentWaveCount = 0;
+    public List<GameObject> remainingEntities;
+
+    void Start()
+    {
+        remainingEntities = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (remainingEntities.Count == 0) {
+            Debug.Log("Spawning Zombies");
+            currentWaveCount++;
+            SpawnZombies(currentWaveCount);
+        }
+    }
+
+    void SpawnZombies(int currentWave) {
+        for(int i = 0; i < spawnPoints.Length; i++) {
+            int entityIndex = Random.Range(0, availableEntities.Length);
+            GameObject currentEntity = Instantiate(availableEntities[entityIndex], spawnPoints[i].transform.position, Quaternion.identity);
+            remainingEntities.Add(currentEntity);
+            Debug.Log("Spawned");
+        }
+    }   
+}
---
+++ Assets/Scripts/ZombieAI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 destination;
+    void Start()
+    {
+        destination = new Vector3(0.0f,0.0f,0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * 1.5f);
+    }
+}
