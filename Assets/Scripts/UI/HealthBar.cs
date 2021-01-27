eedf9df7f4acebd94c9faa160dddbdcfc127b189
Louis <louisemard17@gmail.com>
2021-01-27 16:36:43 +0100

    added life and game over

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public static Image healthBarImage;
+    public static Text healthText;
+
+    void Start()
+    {
+        healthText = GetComponentsInChildren<Text>()[0];
+        healthBarImage = GetComponentsInChildren<Image>()[0];
+        healthBarImage.color = Color.red;
+    }
+
+    public static void setHealthBarInfo(int currentHealth, int maxHealth) {
+        healthBarImage.fillAmount =(float)currentHealth / maxHealth;
+        Debug.Log(maxHealth);
+        healthText.text = currentHealth + " / " + maxHealth;
+    }
+}
